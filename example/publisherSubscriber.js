ab19fbe04ec76315209ecd624215cec2f8643f49
Sean McGary <sean@seanmcgary.com>
2015-08-19 09:56:33 -0700

    queue implementation

--- example/publisherSubscriber.js
+++ example/publisherSubscriber.js
@@ -2,14 +2,29 @@ var RabbitMQ = require('../');
 
 var queue = RabbitMQ.createServer({
 	hostname: 'localhost',
-	port: 5672
+	port: 5672,
+	connections: [
+		{
+			name: 'dev.testqueue.com',
+			queues: [
+				{
+					name: 'my-test-queue',
+					route: 'my-test-queue'
+				}
+			]
+		}
+	]
 });
 
 
-queue.createExchange({
-	name: 'test-exchange.com'
-})
-.then(function(exchange){
-	console.log(exchange);
-})
+//console.log('queue', queue.getQueue('dev.testqueue.com', 'my-test-queue'));
+
+var Q = queue.getQueue('dev.testqueue.com', 'my-test-queue');
+
+var i = 0;
+setInterval(function(){
+	Q.enqueue({ test: i });
+	i++;
+}, 1000);
+
 
